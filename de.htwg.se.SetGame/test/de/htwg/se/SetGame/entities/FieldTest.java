89506293b91509e62dfd4d356b7e4c949f35be09
rabertol <rabertol@3DLABOR11.in.fhkn.de>
2013-10-11 16:49:11 +0200

    Implements of Test

---
+++ de.htwg.se.SetGame/test/de/htwg/se/SetGame/entities/FieldTest.java
@@ -0,0 +1,14 @@
+package de.htwg.se.SetGame.entities;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FieldTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
