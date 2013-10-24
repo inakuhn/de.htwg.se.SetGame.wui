6d4b4aeb12225cbeb5c9635d18ea89aa6382a9d0
rabertol <rabertol@3DLABOR11.in.fhkn.de>
2013-10-24 15:53:14 +0200

    new verison

--- de.htwg.se.SetGame/src/de/htwg/se/SetGame/Modul/Card.java
+++ de.htwg.se.SetGame/src/de/htwg/se/SetGame/Modul/Card.java
@@ -1,12 +1,10 @@
 package de.htwg.se.SetGame.Modul;
 import java.awt.*;
+import java.util.Map;
 import java.text.Format;
 import java.util.TreeSet;
 public class Card {
 	private Color color;
 	private Format formate;
 	private int number;
-	private TreeSet<Card> card;
-
-	
 }
