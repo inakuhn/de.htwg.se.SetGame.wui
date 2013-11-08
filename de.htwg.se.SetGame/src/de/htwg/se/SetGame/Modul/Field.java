2561e3cd178850344eb865a44cbb0a79f77cfc32
rabertol <rabertol@3DLAB17.in.fhkn.de>
2013-11-08 16:00:37 +0100

    Test field und fiels class

--- de.htwg.se.SetGame/src/de/htwg/se/SetGame/Modul/Field.java
+++ de.htwg.se.SetGame/src/de/htwg/se/SetGame/Modul/Field.java
@@ -7,73 +7,63 @@ import org.hamcrest.CoreMatchers;
 import de.htwg.se.SetGame.Modul.Card.Cards;
 
 public class Field {
-	
+
 	private int fieldsize;
 	private int counter = 0;
 	private boolean free = true;
 	private final int max = 81;
 	private Card card = new Card();
 	private Cards[] c = card.pack;
-	
-	
-	public Field(){
+
+	public Field() {
 		init();
 	}
-	
+
 	public Cards[] init() {
 		int fieldsize = 12;
-		Cards[] field =  new Cards[fieldsize];
-		
+		Cards[] field = new Cards[fieldsize];
 
 		this.fieldsize = fieldsize;
 		return fillField(field);
 	}
-	
-	public int[] rand(){
+
+	public int[] rand() {
 		int[] array = new int[81];
-		boolean b =  true;
-		for(int i = 0 ; i < 81; i++){
-			int j = (int) (Math.random()*81+1);
-			for(int t = 0; t < 81; t++){
-				if(j == array[t] && i > 0){
-					j = (int) (Math.random()*81+1);
+		boolean b;
+		for (int i = 0; i < 81; i++) {
+			b = true;
+			int j = (int) (Math.random() * 81 + 1);
+			for (int t = 0; t < 81; t++) {
+				if (j == array[t] && i > 0) {
+					i = i - 1;
+					b = false;
 				}
-					
 			}
+			if (b == true)
 				array[i] = j;
-
-
-
-			
-			
-		}
-		for(int t = 0; t < 81; t++){
-			
 		}
 		return array;
-			
 	}
+
 	private Cards[] fillField(Cards[] f) {
-		
-		
+
 		int[] array = rand();
-		
-		for(int i = 0; i < fieldsize; i++) {
-			
-			f[i] = card.pack[array[i]];	
-		
-			
+
+		for (int i = 0; i < fieldsize; i++) {
+
+			f[i] = card.pack[array[i]];
+
 		}
-		
-		return f ;
-		
+
+		return f;
+
 	}
-	
+
 	private boolean filledField(Cards[] F) {
-		if(F == null) {
+		if (F == null) {
 			return true;
 		}
-		if( F.length == fieldsize+1 ){
+		if (F.length == fieldsize + 1) {
 			return false;
 		} else {
 			return true;
--- de.htwg.se.SetGame/test/de/htwg/se/SetGame/Modul/FieldTest.java
+++ de.htwg.se.SetGame/test/de/htwg/se/SetGame/Modul/FieldTest.java
@@ -7,41 +7,61 @@ import org.junit.Test;
 
 public class FieldTest {
 	Field f;
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		this.f = new Field();
-		
-		
-		
+
 	}
-	
-	
+
 	@Test
 	public void test() {
-		
+
 	}
+
 	@Test
-	public void testFieldradom(){
+	public void testFieldradom() {
 		int array[] = f.rand();
-		int numberofArray = 0;
-		for(int i = 0; i < array.length;i++){
-			for(int t = 0 ; t < array.length;t++){
-				if(t == array[i]){
-					System.out.println(i+"  number: "+t + " in index: "+i);
-					numberofArray++;
-					
+		// int numberofArray = 0;
+		// for(int i = 0; i < array.length;i++){
+		// for(int t = 0 ; t < array.length;t++){
+		// if(t == array[i]){
+		// System.out.println(i+"  number: "+t + " in index: "+i);
+		// numberofArray++;
+		//
+		// }
+		//
+		// }
+		//
+		// }
+		// if(numberofArray == array.length){
+		// System.out.println("true");
+		// }else{
+		// System.out.println("false!! \n numberofarray = "+numberofArray+"\narray length:  "+
+		// array.length );
+		// }
+		//
+		//
+
+		boolean vertauscht;
+		for (int i = array.length - 1; i >= 0; i--) {
+			vertauscht = false;
+			// Durchlauf des Felds von 0 bis i:
+			for (int j = 0; j < i; j++) {
+				if (array[j] > array[j + 1]) {
+					int t = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = t;
+					vertauscht = true;
 				}
-				
 			}
-				
+			if (!vertauscht)
+				break;
 		}
-		if(numberofArray == array.length){
-			System.out.println("true");
-		}else{
-			System.out.println("false!! \n numberofarray = "+numberofArray+"\narray length:  "+ array.length );
+		for(int i = 0 ; i < array.length; i++){
+			System.out.println("array["+i+"] = "+array[i]);
 		}
-			
-		
+
 	}
 
 }
