1408bc13ee7a3cbbaf92fc2d42020378c5cf87a3
raina <raina@192.168.0.100>
2013-12-30 19:32:04 +0100

    playerModus

--- de.htwg.se.SetGame/src/setgame/controller/PlayerComputerModus.java
+++ de.htwg.se.SetGame/src/setgame/controller/PlayerComputerModus.java
@@ -1,5 +1,19 @@
 package setgame.controller;
 
 public class PlayerComputerModus {
+	private int anzahlPlayer;
+	public PlayerComputerModus(int anzahlPlayer){
+		this.anzahlPlayer = anzahlPlayer;
+	}
+	public boolean isComputerModus(){
+		if(this.anzahlPlayer == 1){
+			startTime();
+		}
+		return false;
+	}
+	private void startTime() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
