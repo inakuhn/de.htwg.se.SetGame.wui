1991770ffc48e70cc6a8b2653549a9b82f0c9a86
raina <raina@Raina>
2014-01-16 01:59:38 +0100

    d

--- de.htwg.se.SetGame/src/de/htwg/setgame/SetGame.java
+++ de.htwg.se.SetGame/src/de/htwg/setgame/SetGame.java
@@ -1,5 +1,22 @@
 package de.htwg.setgame;
+import setgame.aview.tui.TextUI;
+import setgame.controller.IController;
+import org.apache.*;
 
-public class SetGame {
+import java.util.Scanner;
 
+public final class SetGame {
+	private static Scanner scanner;
+	private static TextUI tui;
+	private IController controller;
+	private static SetGame instance = null;
+	
+	public static SetGame getInstance(){
+		if(instance == null)
+			new SetGame();
+		return instance;
+	}
+	private SetGame(){
+		
+	}
 }
