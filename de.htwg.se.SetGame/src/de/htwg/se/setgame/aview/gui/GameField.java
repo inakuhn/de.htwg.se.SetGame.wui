12914021fc6c4a659d228965e60bf9f79cb8aee6
Dave <Dave@Dave-PC>
2014-01-16 16:27:04 +0100

    Update Card

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/aview/gui/GameField.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/aview/gui/GameField.java
@@ -118,7 +118,7 @@ public class GameField extends JPanel implements ActionListener {
 		cardToPicture = new TreeMap<>();
 		
 		for (int i = 0; i < pack.getPack().size(); i++) {
-			test.add("/card/" + i + ".gif");
+			test.add("/pack/" + i + ".gif");
 		}
 		for (int index1 = 0; index1 < pack.getPack().size(); index1++) {
 			cardToPicture.put(index1, test.get(index1));
