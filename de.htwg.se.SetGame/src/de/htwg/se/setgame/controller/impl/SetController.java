3c1f3f1ee8a1ab8d98b851a57e1169a4c2945fce
raina <raina@Raina>
2014-01-17 06:32:49 +0100

    commensts

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/controller/impl/SetController.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/controller/impl/SetController.java
@@ -14,19 +14,47 @@ import de.htwg.se.setgame.modell.impl.Field;
 import de.htwg.se.setgame.util.observer.Observable;
 
 
+
 /**
- * @author Raina & David Logic class for the game
- * 
+ * @author raina
+ *
  */
 public class SetController extends Observable implements IController {
+	/**
+	 * 
+	 */
 	private IField field;
+	/**
+	 * 
+	 */
 	private int counter;
+	/**
+	 * 
+	 */
 	private static final int NUMBEROFSETCARDS = 3;
+	/**
+	 * 
+	 */
 	private static final int THREE = 3;
+	/**
+	 * 
+	 */
 	private static final int THOUSAND = 1000;
+	/**
+	 * 
+	 */
 	private final int playerOne;
+	/**
+	 * 
+	 */
 	private final int playerTwo;
+	/**
+	 * 
+	 */
 	private int playerOneCounter;
+	/**
+	 * 
+	 */
 	private int playerTwoCounter;
 
 	/**
@@ -53,6 +81,9 @@ public class SetController extends Observable implements IController {
 		notifyObservers();
 	}
 
+	/**
+	 * 
+	 */
 	private void checkIfIsASeTInGame() {
 		List<ICard> liste = new LinkedList<ICard>();
 		liste.addAll(getSet(this.field.getAllCardsInGame()));
@@ -82,6 +113,12 @@ public class SetController extends Observable implements IController {
 	 * @param cardThree
 	 * @return true if all the cards are in the field is only a safety Method
 	 */
+	/**
+	 * @param cardOne
+	 * @param cardTwo
+	 * @param cardThree
+	 * @return
+	 */
 	private boolean isInFiel(ICard cardOne, ICard cardTwo, ICard cardThree) {
 		this.counter = 0;
 		for (ICard card : field.getCardsInField()) {
@@ -103,6 +140,12 @@ public class SetController extends Observable implements IController {
 	 * @param cardThree
 	 * @return return true if is a set.
 	 */
+	/**
+	 * @param cardOne
+	 * @param cardTwo
+	 * @param cardThree
+	 * @return
+	 */
 	private boolean isAset(ICard cardOne, ICard cardTwo, ICard cardThree) {
 
 		if (!isInFiel(cardOne, cardTwo, cardThree)) {
@@ -122,8 +165,8 @@ public class SetController extends Observable implements IController {
 	}
 
 	/**
-	 * @param list
-	 *            is the Cards in field the new ones if there is no set anymore.
+	 * @param list is the Cards in field the new ones if there is no set anymore.
+	 * @return true if still set in fields
 	 */
 	private boolean alltheSetsInField(List<ICard> list) {
 		if (!getSet(list).isEmpty()) {
@@ -147,6 +190,9 @@ public class SetController extends Observable implements IController {
 	/**
 	 * changed the Cards in the field if necessary. to
 	 */
+	/**
+	 * @return
+	 */
 	private boolean changeCardsinGame() {
 		List<ICard> allCards = new LinkedList<ICard>();
 		allCards.addAll(field.getAllCardsInGame());
@@ -158,11 +204,23 @@ public class SetController extends Observable implements IController {
 
 	}
 
+	/**
+	 * @param cardOne
+	 * @param cardTwo
+	 * @param cardThree
+	 * @return
+	 */
 	private boolean proveColor(ICard cardOne, ICard cardTwo, ICard cardThree) {
 		return proveString(cardOne.getColor(), cardTwo.getColor(),
 				cardThree.getColor());
 	}
 
+	/**
+	 * @param cardOne
+	 * @param cardTwo
+	 * @param cardThree
+	 * @return
+	 */
 	private boolean proveNumberOfComponents(ICard cardOne, ICard cardTwo,
 			ICard cardThree) {
 		if (cardOne.getNumberOfComponents() == cardTwo.getNumberOfComponents()
@@ -180,16 +238,34 @@ public class SetController extends Observable implements IController {
 		return false;
 	}
 
+	/**
+	 * @param cardOne
+	 * @param cardTwo
+	 * @param cardThree
+	 * @return
+	 */
 	private boolean proveFilling(ICard cardOne, ICard cardTwo, ICard cardThree) {
 		return proveString(cardOne.getPanelFilling(),
 				cardTwo.getPanelFilling(), cardThree.getPanelFilling());
 	}
 
+	/**
+	 * @param cardOne
+	 * @param cardTwo
+	 * @param cardThree
+	 * @return
+	 */
 	private boolean proveForm(ICard cardOne, ICard cardTwo, ICard cardThree) {
 		return proveString(cardOne.getForm(), cardTwo.getForm(),
 				cardThree.getForm());
 	}
 
+	/**
+	 * @param stringOne
+	 * @param stringTwo
+	 * @param stringThree
+	 * @return
+	 */
 	private boolean proveString(String stringOne, String stringTwo,
 			String stringThree) {
 		if (stringOne.compareTo(stringTwo) == 0
@@ -203,6 +279,12 @@ public class SetController extends Observable implements IController {
 		return false;
 	}
 
+	/**
+	 * @param cardOne
+	 * @param cardTwo
+	 * @param cardThree
+	 * @return
+	 */
 	private boolean proveIfIsASet(ICard cardOne, ICard cardTwo, ICard cardThree) {
 		if (proveColor(cardOne, cardTwo, cardThree)
 				&& proveFilling(cardOne, cardTwo, cardThree)
@@ -214,6 +296,10 @@ public class SetController extends Observable implements IController {
 
 	}
 
+	/**
+	 * @param list
+	 * @return
+	 */
 	private List<ICard> getSet(List<ICard> list) {
 		LinkedList<ICard> setList = new LinkedList<ICard>();
 		if (list.size() >= NUMBEROFSETCARDS) {
