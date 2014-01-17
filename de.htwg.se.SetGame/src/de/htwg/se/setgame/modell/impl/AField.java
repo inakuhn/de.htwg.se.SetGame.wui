7415883bf7fdf9791017ff1aaf1f1b5e3811c383
raina <raina@Raina>
2014-01-17 04:19:00 +0100

    ss

--- de.htwg.se.SetGame/src/de/htwg/se/setgame/modell/impl/AField.java
+++ de.htwg.se.SetGame/src/de/htwg/se/setgame/modell/impl/AField.java
@@ -1,9 +1,8 @@
 package de.htwg.se.setgame.modell.impl;
 
-import java.util.List;
-import java.util.Map;
 
-import de.htwg.se.setgame.modell.IField;
+
+
 
 public abstract class AField implements IField {
 
