a2974a4c496b5238067cb72cba2e187a2c1632f8
Philipp Daniels <phdaniel@htwg-konstanz.de>
2014-10-22 14:54:41 +0200

    Extend to have access to Controller at view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,19 @@
 package controllers;
 
+import de.htwg.se.setgame.SetGame;
+import de.htwg.se.setgame.aview.tui.TextUI;
+import de.htwg.se.setgame.controller.IController;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.lang.String;
+
 public class Application extends Controller {
+
+    static IController controller = SetGame.getInstance().getIController();
     
     public static Result index() {
-
-
-
-        return ok(views.html.index.render("Hello Play Framework"));
+        return ok(views.html.index.render(TextUI.MESSAGE_WELCOME, controller));
     }
     
 }
