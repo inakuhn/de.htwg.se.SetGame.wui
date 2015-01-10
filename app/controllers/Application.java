c896ee89fb5795d550b3e2cdfeb1c3c4ba407288
rabertol <raina.bertolini@gmail.com>
2015-01-10 17:44:08 +0100

    Solve method

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -6,13 +6,17 @@ import de.htwg.se.setgame.controller.IController;
 import de.htwg.se.setgame.modell.ICard;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.mvc.WebSocket;
+import views.html.solve;
 
 import java.lang.Integer;
 import java.lang.Package;
 import java.lang.String;
 import java.lang.System;
+import java.util.HashMap;
 import java.util.List;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Application extends Controller {
 
@@ -21,22 +25,32 @@ public class Application extends Controller {
     public static Result index() {
         return ok(views.html.index.render(controller, getField()));
     }
+    public static Result solve() {
+        List<Integer> result = new LinkedList<>();
+        List<ICard> setInGame = controller.getASetInGame();
+        Map<Integer,ICard> indexAndCard = getIndexAndCard();
+            for (ICard setCard : setInGame) {
+                  for(Integer cardIndex: indexAndCard.keySet()){
+                      if(indexAndCard.get(cardIndex).comparTo(setCard)){
+                          result.add(cardIndex);
+                  }
+            }
+        }
+        System.out.println("Solved List:");
+        System.out.println(result);
+        return ok(views.html.solve.render(controller, getField(), result));
+    }
 
     public static Result help() {
         return ok(views.html.help.render());
     }
 
+
     private static List<Integer> getField() {
         List<ICard> packList = controller.getPack().getPack();
         List<Integer> result = new LinkedList<Integer>();
 
-        for (ICard card:controller.getCardInFieldGame()) {
-            for (int i = 0; i < packList.size(); i++) {
-                if (card.comparTo(packList.get(i))) {
-                    result.add(i);
-                }
-            }
-        }
+        result.addAll(getIndexAndCard().keySet());
 
         return result;
     }
@@ -50,12 +64,26 @@ public class Application extends Controller {
         ICard first = allCardsInField.get(cardOne);
         ICard sec = allCardsInField.get(cardTwo);
         ICard th = allCardsInField.get(cardThree);
-        controller.isASetForController(first,sec,th,player);
+        controller.isASetForController(first, sec, th, player);
         return ok(views.html.index.render(controller, getField()));
     }
 
     public static Result reset() {
         controller.newGame();
         return ok(views.html.index.render(controller, getField()));
+    }
+    private static Map<Integer,ICard> getIndexAndCard(){
+        List<ICard> packList = controller.getPack().getPack();
+        Map<Integer,ICard> result = new HashMap<Integer,ICard>();
+
+        for (ICard card:controller.getCardInFieldGame()) {
+            for (int i = 0; i < packList.size(); i++) {
+                if (card.comparTo(packList.get(i))) {
+                    result.put(i,packList.get(i));
+                }
+            }
+        }
+
+        return result;
     }
 }
