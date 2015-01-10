58e9b081b24ce9a4e1d567c1a5aabe3963842756
Philipp Daniels <philipp.daniels@gmail.com>
2015-01-10 14:34:03 +0100

    Build List with fields cards

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -3,17 +3,38 @@ package controllers;
 import de.htwg.se.setgame.SetGame;
 import de.htwg.se.setgame.aview.tui.TextUI;
 import de.htwg.se.setgame.controller.IController;
+import de.htwg.se.setgame.modell.ICard;
 import play.mvc.Controller;
 import play.mvc.Result;
 
+import java.lang.Integer;
+import java.lang.Package;
 import java.lang.String;
+import java.lang.System;
+import java.util.List;
+import java.util.LinkedList;
 
 public class Application extends Controller {
 
     static IController controller = SetGame.getInstance(false).getIController();
     
     public static Result index() {
-        return ok(views.html.index.render(TextUI.MESSAGE_WELCOME, controller));
+        return ok(views.html.index.render(controller, getField()));
+    }
+
+    private static List<Integer> getField() {
+        List<ICard> packList = controller.getPack().getPack();
+        List<Integer> result = new LinkedList<Integer>();
+
+        for (ICard card:controller.getCardInFieldGame()) {
+            for (int i = 0; i < packList.size(); i++) {
+                if (card.comparTo(packList.get(i))) {
+                    result.add(i);
+                }
+            }
+        }
+
+        return result;
     }
     
 }
