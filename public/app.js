2046ead76b8fe538fe1f60c584b403c9d2b96fd4
Philipp Daniels <philipp.daniels@gmail.com>
2015-01-12 00:10:14 +0100

    Remove ng-controller attribute to avoid double loaded controller

--- public/app.js
+++ public/app.js
@@ -40,59 +40,57 @@
             });
         }
 
-        $scope.init = function () {
-            $scope.loadData();
+        $scope.loadData();
 
-            var mode = 1; // 1=Press button, 2=Select set
-            var player;
+        var mode = 1; // 1=Press button, 2=Select set
+        var player;
 
-            var $main = $('.js-field');
-            var $pressArea = $('body');
-            var $modalTurn = $main.find('.js-turn');
-            var $modalSetGood = $main.find('.js-set-good');
-            var $modalSetBad = $main.find('.js-set-bad');
-            var $cards = $main.find('.js-cards');
+        var $main = $('.js-field');
+        var $pressArea = $('body');
+        var $modalTurn = $main.find('.js-turn');
+        var $modalSetGood = $main.find('.js-set-good');
+        var $modalSetBad = $main.find('.js-set-bad');
+        var $cards = $main.find('.js-cards');
 
-            /////////////////////////////////
-            // Player selection
-            /////////////////////////////////
-            $pressArea.keypress(function(e) {
-                $main.find('button').each(function() {
-                    if ($(this).data('key') == e.which && mode === 1) {
-                        player = $(this).data('player');
-                        mode = 2;
+        /////////////////////////////////
+        // Player selection
+        /////////////////////////////////
+        $pressArea.keypress(function(e) {
+            $main.find('button').each(function() {
+                if ($(this).data('key') == e.which && mode === 1) {
+                    player = $(this).data('player');
+                    mode = 2;
 
-                        $modalTurn.find('.js-name').text($(this).data('name'));
-                        $modalTurn.modal();
-                    }
+                    $modalTurn.find('.js-name').text($(this).data('name'));
+                    $modalTurn.modal();
+                }
+            });
+        });
+
+        /////////////////////////////////
+        // Card selection
+        /////////////////////////////////
+        $main.on('click', 'img', function() {
+            var cssClass = 'selected';
+            if (mode == 2) {
+                $(this).addClass(cssClass);
+            }
+
+            var $selectedCards = $main.find('img.'+cssClass);
+
+            if ($selectedCards.length == 3) {
+                var url = '/set/' + player + '/' + get(0) + '/' + get(1) + '/' + get(2);
+                $.get(url, function(data) {
+                    (data) ? $modalSetGood.modal() : $modalSetBad.modal();
                 });
-            });
 
-            /////////////////////////////////
-            // Card selection
-            /////////////////////////////////
-            $main.on('click', 'img', function() {
-                var cssClass = 'selected';
-                if (mode == 2) {
-                    $(this).addClass(cssClass);
-                }
+                $selectedCards.removeClass(cssClass);
+                mode = 1;
+            }
 
-                var $selectedCards = $main.find('img.'+cssClass);
-
-                if ($selectedCards.length == 3) {
-                    var url = '/set/' + player + '/' + get(0) + '/' + get(1) + '/' + get(2);
-                    $.get(url, function(data) {
-                        (data) ? $modalSetGood.modal() : $modalSetBad.modal();
-                    });
-
-                    $selectedCards.removeClass(cssClass);
-                    mode = 1;
-                }
-
-                function get(i) {
-                    return $selectedCards.eq(i).data('field');
-                }
-            });
-        }
+            function get(i) {
+                return $selectedCards.eq(i).data('field');
+            }
+        });
     });
 })(jQuery, location, angular);
