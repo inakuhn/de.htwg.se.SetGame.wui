8eb23788b0721ca3b856b0e7677942ea5ac16656
Philipp Daniels <philipp.daniels@gmail.com>
2015-01-11 23:55:19 +0100

    Moved current frontend logic into angular controller

--- public/app.js
+++ public/app.js
@@ -1,62 +1,11 @@
 (function($, l, a){
-    var mode = 1; // 1=Press button, 2=Select set
-    var player;
-
-    var $main = $('.js-field');
-    var $pressArea = $('body');
-    var $modalTurn = $main.find('.js-turn');
-    var $modalSetGood = $main.find('.js-set-good');
-    var $modalSetBad = $main.find('.js-set-bad');
-    var $cards = $main.find('.js-cards');
-
-    /////////////////////////////////
-    // Player selection
-    /////////////////////////////////
-    $pressArea.keypress(function(e) {
-        $main.find('button').each(function() {
-            if ($(this).data('key') == e.which && mode === 1) {
-                player = $(this).data('player');
-                mode = 2;
-
-                $modalTurn.find('.js-name').text($(this).data('name'));
-                $modalTurn.modal();
-            }
-        });
-    });
-
-    /////////////////////////////////
-    // Card selection
-    /////////////////////////////////
-    $main.on('click', 'img', function() {
-        var cssClass = 'selected';
-        if (mode == 2) {
-            $(this).addClass(cssClass);
-        }
-
-        var $selectedCards = $main.find('img.'+cssClass);
-
-        if ($selectedCards.length == 3) {
-            var url = '/set/' + player + '/' + get(0) + '/' + get(1) + '/' + get(2);
-            $.get(url, function(data) {
-                (data) ? $modalSetGood.modal() : $modalSetBad.modal();
-            });
-
-            $selectedCards.removeClass(cssClass);
-            mode = 1;
-        }
-
-        function get(i) {
-            return $selectedCards.eq(i).data('field');
-        }
-    });
-
     /////////////////////////////////
     // WebSocket
     /////////////////////////////////
     var ws = $.gracefulWebSocket('ws://' + l.host + '/ws');
     ws.onmessage = function (event) {
-        var $scope = angular.element($cards[0]).scope();
-        $scope.loadData();
+        var $main = $('.js-field');
+        angular.element($main.find('.js-cards')[0]).scope().loadData();
     };
 
     /////////////////////////////////
@@ -78,14 +27,66 @@
             });
     });
 
-    app.controller('CardCtrl', function ($scope, $http){
+    app.controller('CardCtrl', function ($scope, $http) {
         $scope.loadData = function() {
             $http.get('/cards.json').success(function(data) {
                 $scope.cards = data;
             });
         }
 
-        //Initial load
-        $scope.loadData();
+        $scope.init = function () {
+            $scope.loadData();
+
+            var mode = 1; // 1=Press button, 2=Select set
+            var player;
+
+            var $main = $('.js-field');
+            var $pressArea = $('body');
+            var $modalTurn = $main.find('.js-turn');
+            var $modalSetGood = $main.find('.js-set-good');
+            var $modalSetBad = $main.find('.js-set-bad');
+            var $cards = $main.find('.js-cards');
+
+            /////////////////////////////////
+            // Player selection
+            /////////////////////////////////
+            $pressArea.keypress(function(e) {
+                $main.find('button').each(function() {
+                    if ($(this).data('key') == e.which && mode === 1) {
+                        player = $(this).data('player');
+                        mode = 2;
+
+                        $modalTurn.find('.js-name').text($(this).data('name'));
+                        $modalTurn.modal();
+                    }
+                });
+            });
+
+            /////////////////////////////////
+            // Card selection
+            /////////////////////////////////
+            $main.on('click', 'img', function() {
+                var cssClass = 'selected';
+                if (mode == 2) {
+                    $(this).addClass(cssClass);
+                }
+
+                var $selectedCards = $main.find('img.'+cssClass);
+
+                if ($selectedCards.length == 3) {
+                    var url = '/set/' + player + '/' + get(0) + '/' + get(1) + '/' + get(2);
+                    $.get(url, function(data) {
+                        (data) ? $modalSetGood.modal() : $modalSetBad.modal();
+                    });
+
+                    $selectedCards.removeClass(cssClass);
+                    mode = 1;
+                }
+
+                function get(i) {
+                    return $selectedCards.eq(i).data('field');
+                }
+            });
+        }
     });
 })(jQuery, location, angular);
